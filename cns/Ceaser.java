bec95fe4ca736e67fbcdab663fec7ad5da5ba498
cseaboys <cseaboys2026@gmail.com>
2025-01-25 11:50:32 +0530

    cns 3-1

---
+++ cns/Ceaser.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Ceaser {
+
+    static String encrypt(String txt, int key) {
+        StringBuilder enc = new StringBuilder();
+        txt.chars().forEach(c -> {
+            enc.append(Character.isLetter(c)
+                    ? (char) ((c - 'a' + key) % 26 + 'a')
+                    : (char) c);
+        });
+        return enc.toString();
+    }
+
+    static String decrypt(String txt, int key) {
+        StringBuilder enc = new StringBuilder();
+        txt.chars().forEach(c -> {
+            enc.append(Character.isLetter(c)
+                    ? (char) ((c - 'a' - key) % 26 + 'a')
+                    : (char) c);
+        });
+        return enc.toString();
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String txt = sc.nextLine().toLowerCase();
+
+        int key = sc.nextInt();
+        key %= 26;
+        var enc = encrypt(txt, key);
+        var dec = decrypt(enc, key);
+        System.out.println(enc+ "\n"+ dec);
+        sc.close();
+
+    }
+}
