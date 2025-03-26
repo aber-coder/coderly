604ed8c46950d71fd2cca37e948beb2756262ee9
cseaboys <cseaboys2026@gmail.com>
2025-03-26 11:31:26 +0530

    modified:   mean/e7/7a/app.js

--- mean/e7/7a/app.js
+++ mean/e7/7a/app.js
@@ -2,6 +2,6 @@ const express = require("express");
 const app = express();
 const things = require("./route");
 app.use("/things", things);
-app.listen(8000, "127.0.0.1", () => {
-  console.log("Server is running on http://127.0.0.1:8000");
+app.listen(8000, "localhost", () => {
+  console.log("Server is running on http://localhost:8000");
 });
--- mean/e7/7a/route.js
+++ mean/e7/7a/route.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const router = express.Router();
+
 router.get("/", (req, res) => {
   res.send("Things - get data");
 });
+
 router.post("/", (req, res) => {
-  res.send("hello world");
+  res.send("hello, got a post request");
 });
+
 module.exports = router;
